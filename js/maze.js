769fdfb77a4113d320649d15741a896e16ada05a
joshr9 <joshruchwarger@gmail.com>
2016-07-13 12:43:52 -0600

    Initial commit

---
+++ js/maze.js
@@ -0,0 +1,11 @@
+function Maze(width, height) {
+  this.width = width
+  this.height = height
+
+  this.startX = null
+  this.startY = null
+  this.startOrientation = null
+  this.endX = null
+  this.endY = null
+
+}
