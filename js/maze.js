acb5f92b1335f4e46ea20c9df146093ee35535e8
joshr9 <joshruchwarger@gmail.com>
2016-07-13 13:21:00 -0600

    Added style to represent maze on http-server

--- js/maze.js
+++ js/maze.js
@@ -8,4 +8,38 @@ function Maze(width, height) {
   this.endX = null
   this.endY = null
 
+  this.spaces = []
+
+  var x, y
+  //goes through each column and creates an array element for it
+  //That array element will be another empty array
+  for (x = 1; x <= width; x++){
+    this.spaces[x] = []
+    //add one element to the array for each space in the column
+    for(y = 1; y <= height; y++){
+      this.spaces[x][y] = new MazeSpace
+    }
+  }
+
+}
+
+// Defines a method called setStart that will be available in all of our maze objects
+Maze.prototype.setStart = function(x, y, orientation) {
+  this.startX = x
+  this.startY = y
+  this.startOrientation = orientation
+}
+ // AKA 2 setter methods to set all 5 of these properties
+Maze.prototype.setEnd = function(x, y) {
+  this.endX = x
+  this.endY = y
+}
+
+Maze.prototype.setWall = function(x, y, direction) {
+  if(x > 0 && x<= this.width && y > 0 && y <= this.height &&
+  ["north", "east", "south", "west"].indexOf(direction) !== -1) {
+    this.spaces[x][y].setWall(direction)
+    return true
+  }
+  return false
 }
---
+++ js/mazespace.js
@@ -0,0 +1,10 @@
+function MazeSpace() {
+  this.north = false
+  this.east = false
+  this.south = false
+  this.west = false
+}
+
+MazeSpace.prototype.setWall = function(direction) {
+  this[direction] = true
+}
